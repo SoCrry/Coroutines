32b746cc445a882fad236d41602df24cfe595f2b
Karen Pashoyan <karen-pashoyan@mail.ru>
2024-07-25 16:40:19 +0500

    initial commit

---
+++ src/main/kotlin/dto/CommentWithAuthor.kt
@@ -0,0 +1,6 @@
+package ru.netology.dto
+
+data class CommentWithAuthor(
+    val comment: Comment,
+    val author: Author,
+)
---
+++ src/main/kotlin/dto/PostWithComments.kt
@@ -0,0 +1,7 @@
+package ru.netology.dto
+
+data class PostWithComments(
+    val post: Post,
+    val author: Author,
+    val comments: List<Comment>,
+)
